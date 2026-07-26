47453add1b8062bf57eee7a351d4c9e20871d71e
indraneel10 <sanyalindraneel1@gmail.com>
2026-07-26 09:45:59 +0530

    Update test_connection.py

--- backend/test_connection.py
+++ backend/test_connection.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, text
 from app.config.settings import settings
 
+print("DATABASE_URL =", settings.DATABASE_URL)
+
 engine = create_engine(settings.DATABASE_URL)
 
 with engine.connect() as conn:
