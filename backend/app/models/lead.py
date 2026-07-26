88f777de5a82e87d797b2c9587e5f9b598cf18f0
indraneel10 <sanyalindraneel1@gmail.com>
2026-07-26 10:05:37 +0530

    Update lead.py

--- backend/app/models/lead.py
+++ backend/app/models/lead.py
@@ -1,26 +1,35 @@
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+)
+from datetime import datetime
 
 from app.database.base import Base
 
 
 class Lead(Base):
-
     __tablename__ = "leads"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
 
-    phone_number = Column(String)
+    customer_name = Column(String(100))
 
-    language = Column(String)
+    phone_number = Column(String(20), unique=True)
 
-    property_type = Column(String)
+    language = Column(String(20))
 
-    bhk = Column(String)
+    property_type = Column(String(20))
+
+    bhk = Column(String(20))
+
+    rooms = Column(Integer)
 
     budget = Column(Integer)
 
-    package = Column(String)
+    package = Column(String(20))
 
-    status = Column(String)
+    status = Column(String(30), default="NEW")
+
+    created_at = Column(DateTime, default=datetime.utcnow)
